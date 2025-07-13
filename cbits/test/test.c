0ea3e0215f94b868325ae4f9364a3b010bc5fa40
ftzm <m@ftzm.org>
2025-07-13 21:32:56 +0200

    all suites

--- cbits/test/test.c
+++ cbits/test/test.c
@@ -1,6 +1,15 @@
 #include "greatest.h"
 
 SUITE_EXTERN(capture_suite);
+SUITE_EXTERN(corner_moves_1_suite);
+SUITE_EXTERN(corner_moves_2_suite);
+SUITE_EXTERN(corner_paths_1_suite);
+SUITE_EXTERN(corner_paths_2_suite);
+SUITE_EXTERN(move_suite);
+SUITE_EXTERN(position_set_suite);
+SUITE_EXTERN(score_suite);
+SUITE_EXTERN(victory_suite);
+SUITE_EXTERN(zobrist_suite);
 
 GREATEST_MAIN_DEFS();
 
@@ -8,5 +17,14 @@ int main(int argc, char **argv) {
 
   GREATEST_MAIN_BEGIN();
   RUN_SUITE(capture_suite);
+  RUN_SUITE(corner_moves_1_suite);
+  RUN_SUITE(corner_moves_2_suite);
+  RUN_SUITE(corner_paths_1_suite);
+  RUN_SUITE(corner_paths_2_suite);
+  RUN_SUITE(move_suite);
+  RUN_SUITE(position_set_suite);
+  RUN_SUITE(score_suite);
+  RUN_SUITE(victory_suite);
+  RUN_SUITE(zobrist_suite);
   GREATEST_MAIN_END();
 }
