13dbbd562cf6243a08d9fb04c3774150cb8290be
ftzm <m@ftzm.org>
2025-07-18 14:58:31 +0200

    quiesce wip

--- cbits/src/search.c
+++ cbits/src/search.c
@@ -1,9 +1,12 @@
-#include "position_set.h"
 #include "board.h"
 #include "layer.h"
 #include "move.h"
-#include "stdbool.h"
+#include "position_set.h"
 #include "score.h"
+#include "stdbool.h"
+#include "assert.h"
+#include "stdint.h"
+#include "move_legacy.h"
 
 // typedef struct negamax_ab_result {
 //   move _move;
@@ -11,7 +14,7 @@
 //   i32 _score;
 //   score_state ss;
 // } negamax_ab_result;
-// 
+//
 // // move moves_table[11][240];
 // // board boards_table[11][240];
 // // board scores_table[11][240];
@@ -22,7 +25,7 @@
 // move PREV_PV[MAX_DEPTH];
 // int PREV_PV_LENGTH;
 // move KILLER_MOVES[MAX_DEPTH][2];
-// 
+//
 // i32 negamax_ab_sorted_pv(
 //     const move m,
 //     const board b,
@@ -38,25 +41,25 @@
 //     const bool allow_null_move,
 //     const struct ai_settings &ai_settings) {
 //   PV_LENGTH[ply] = ply;
-// 
+//
 //   /*
 //   if (ply > 31) {
 //     printf("max depth exceeded\n");
 //     exit(1);
 //   }
 //   */
-// 
+//
 //   i32 game_over_score = 0;
 //   bool game_over = game_over_check(b, is_black_turn, game_over_score);
 //   if (game_over) {
 //     return game_over_score;
 //   }
-// 
+//
 //   int total = 0;
 //   move moves_table[324];
 //   board boards_table[324];
 //   u8 cap_counts[324] = {0};
-// 
+//
 //   if (depth > 3 && ply > 0 && allow_null_move && !is_pv) {
 //     // Null move heuristic
 //     // TODO: figure out the most suitable depth cutoff (if any)
@@ -85,7 +88,7 @@
 //   }
 //   /*
 //    */
-// 
+//
 //   if (depth <= 0) {
 //     if (ply < MAX_DEPTH) {
 //       if (is_black_turn) {
@@ -104,35 +107,36 @@
 //       return score_board(b, is_black_turn) + ss.get_score(is_black_turn);
 //     }
 //   } else {
-// 
+//
 //     if (is_black_turn) {
 //       get_team_moves<true>(b, &total, moves_table, cap_counts, boards_table);
 //     } else {
 //       get_king_moves(b, &total, moves_table, cap_counts, boards_table);
-//       get_team_moves<false>(b, &total, moves_table, cap_counts, boards_table);
+//       get_team_moves<false>(b, &total, moves_table, cap_counts,
+//       boards_table);
 //     }
 //   }
-// 
+//
 //   negamax_ab_result combi[324];
 //   for (int i = 0; i < total; i++) {
 //     combi[i] = (negamax_ab_result){moves_table[i], boards_table[i]};
-// 
+//
 //     if (combi[i]._move.orig == 44 && combi[i]._move.dest == 22 &&
 //         ss.se_guard_count == 3) {
 //       print_board(combi[i]._board);
 //     }
-// 
+//
 //     // update score state
 //     combi[i].ss = update_score_state(
 //         ss, combi[i]._move, is_black_turn ? black_type : white_type);
-// 
+//
 //     // update score state piece counts
 //     if (is_black_turn) {
 //       combi[i].ss.white_pawn_count -= cap_counts[i];
 //     } else {
 //       combi[i].ss.black_pawn_count -= cap_counts[i];
 //     }
-// 
+//
 //     // TODO: move this somewhere closer to the core capture code se we don't
 //     // have to re-calculate the indexes of captures
 //     if (cap_counts[i]) {
@@ -164,22 +168,25 @@
 //         }
 //       }
 //     }
-// 
+//
 //     u8 king_pos =
 //         b.king[0] ? _tzcnt_u64(b.king[0]) : _tzcnt_u64(b.king[1]) + 64;
-// 
+//
 //     // update pst score
 //     if (is_black_turn) {
-//       combi[i].ss.pst_black_score -= ai_settings.black_pst[combi[i]._move.orig];
-//       combi[i].ss.pst_black_score += ai_settings.black_pst[combi[i]._move.dest];
+//       combi[i].ss.pst_black_score -=
+//       ai_settings.black_pst[combi[i]._move.orig]; combi[i].ss.pst_black_score
+//       += ai_settings.black_pst[combi[i]._move.dest];
 //     } else if (combi[i]._move.orig == !king_pos) {
-//       combi[i].ss.pst_white_score -= ai_settings.white_pst[combi[i]._move.orig];
-//       combi[i].ss.pst_white_score += ai_settings.white_pst[combi[i]._move.dest];
+//       combi[i].ss.pst_white_score -=
+//       ai_settings.white_pst[combi[i]._move.orig]; combi[i].ss.pst_white_score
+//       += ai_settings.white_pst[combi[i]._move.dest];
 //     } else {
-//       combi[i].ss.pst_king_score -= ai_settings.king_pst[combi[i]._move.orig];
-//       combi[i].ss.pst_king_score += ai_settings.king_pst[combi[i]._move.dest];
+//       combi[i].ss.pst_king_score -=
+//       ai_settings.king_pst[combi[i]._move.orig]; combi[i].ss.pst_king_score
+//       += ai_settings.king_pst[combi[i]._move.dest];
 //     }
-// 
+//
 //     combi[i]._score = depth > 1 ? combi[i].ss.get_score(is_black_turn) : 0;
 //     // Add bonus if killer move
 //     if (combi[i]._move == KILLER_MOVES[ply][0] ||
@@ -190,7 +197,7 @@
 //       combi[i]._score += 100000000;
 //     }
 //   }
-// 
+//
 //   // niave sort
 //   /*
 //   if (depth > 1) {
@@ -199,7 +206,7 @@
 //     });
 //   }
 //   */
-// 
+//
 //   // start with a bogus best
 //   i32 best = MIN_SCORE;
 //   negamax_ab_result tmp;
@@ -217,17 +224,17 @@
 //         combi[best_index] = tmp;
 //       }
 //     }
-// 
+//
 //     // TODO: move this to leaf node
 //     (*tally)++;
-// 
+//
 //     // update and check repetitions
 //     team_repetitions new_r =
 //         update_team_repetitions(r, combi[i]._move, is_black_turn);
 //     if (new_r.over_limit(is_black_turn)) {
 //       continue;
 //     }
-// 
+//
 //     // Late Move Reduction: if we have more than two ply left, and
 //     // we've already examined the best 25 moves based on the move
 //     // ordering heuristics, then search this move 2 ply more shallow
@@ -253,12 +260,12 @@
 //         continue;
 //       }
 //     }
-// 
+//
 //     // calcualte result and negate score the is_pv parameter is (is_pv
 //     // && !i) because if this _is_ the pv then the first entry must be
 //     // the next PV node due to the bonus applied above, unless the.
 //     // next PV move wasn't found, which would be a real bug.
-// 
+//
 //     i32 eval = -negamax_ab_sorted_pv(
 //         combi[i]._move,
 //         combi[i]._board,
@@ -273,7 +280,7 @@
 //         combi[i].ss,
 //         true,
 //         ai_settings);
-// 
+//
 //     /*
 //     i32 eval;
 //     if (i == 0) {
@@ -296,12 +303,13 @@
 //       }
 //     }
 //     */
-// 
-//     // if (depth == 1 && (m.orig == 115 && m.dest == 114)) printf("score: %d\n",
+//
+//     // if (depth == 1 && (m.orig == 115 && m.dest == 114)) printf("score:
+//     %d\n",
 //     // next_result);
 //     // if (depth == 1) printf("score: %d\n", eval);
 //     // if (depth == 1) print_board(combi[i]._board);
-// 
+//
 //     if (eval > best) {
 //       best = eval;
 //     }
@@ -309,14 +317,16 @@
 //       // ALPHA BETA HANDLING
 //       // ---------------------------------------------------------------
 //       alpha = best;
-// 
+//
 //       // PV HANDLING
-//       // --------------------------------------------------------------- assign
+//       // ---------------------------------------------------------------
+//       assign
 //       // move discovered here
 //       PV_TABLE[ply][ply] = combi[i]._move;
 //       PV_TABLE_BOARDS[ply][ply] = combi[i]._board; // me only
 //       // copy up moves discovered at lower depths
-//       for (int next_ply = ply + 1; next_ply < PV_LENGTH[ply + 1]; next_ply++) {
+//       for (int next_ply = ply + 1; next_ply < PV_LENGTH[ply + 1]; next_ply++)
+//       {
 //         /*
 //         if (next_ply > 31) {
 //           printf("next ply over limit\n");
@@ -329,7 +339,7 @@
 //       }
 //       // adjust pv length
 //       PV_LENGTH[ply] = PV_LENGTH[ply + 1];
-// 
+//
 //       // KILLER HANDLING
 //       // ---------------------------------------------------------------
 //       // TODO: exclude captures
@@ -346,10 +356,10 @@
 //   // if (depth == 1 && (m.orig == 115 && m.dest == 114))
 //   // printf("----------------------");
 //   // if (depth == 1) printf("----------------------");
-// 
+//
 //   return best;
 // }
-// 
+//
 // enum Flag : u8 {
 //   lower_bound = 1,
 //   exact = 2,
@@ -361,7 +371,7 @@
 //   i32 s;
 //   team_repetitions r;
 // };
-// 
+//
 // i32 quiesce(
 //     const move m,
 //     const board b,
@@ -378,33 +388,33 @@
 //     const struct ai_settings &ai_settings) {
 //   // base implementation from:
 //   // https://www.chessprogramming.org/Quiescence_Search
-// 
+//
 //   // TODO: implement delta pruning
-// 
-//   // assert we don't exceed a generous ply limit to guard against infinite loops
-//   assert(ply < 20);
-// 
+//
+//   // assert we don't exceed a generous ply limit to guard against infinite
+//   loops assert(ply < 20);
+//
 //   // int stand_pat = Evaluate();
 //   // if( stand_pat >= beta )
 //   //     return beta;
 //   // if( alpha < stand_pat )
 //   //     alpha = stand_pat;
-//   int stand_pat = score_board(b, is_black_turn) + ss.get_score(is_black_turn);
-//   if (stand_pat >= beta)
+//   int stand_pat = score_board(b, is_black_turn) +
+//   ss.get_score(is_black_turn); if (stand_pat >= beta)
 //     return beta;
 //   if (alpha < stand_pat)
 //     alpha = stand_pat;
-// 
+//
 //   // Gen moves
 //   // 1. a) if black, moves that block escape paths
 //   //    b) if white, escape moves
 //   // 2. capture moves
-// 
+//
 //   // until( every_capture_has_been_examined )  {
 //   //     MakeCapture();
 //   //     score = -Quiesce( -beta, -alpha );
 //   //     TakeBackMove();
-// 
+//
 //   //     if( score >= beta )
 //   //         return beta;
 //   //     if( score > alpha )
@@ -412,7 +422,7 @@
 //   // }
 //   // return alpha;
 // }
-// 
+//
 // search_result negamax_ab_sorted_pv_runner(
 //     board b,
 //     team_repetitions r,
@@ -427,7 +437,7 @@
 //   printf("sw: %d\n", s.sw_guard_count);
 //   printf("se: %d\n", s.se_guard_count);
 //   */
-// 
+//
 //   memset(KILLER_MOVES, 0, MAX_DEPTH * sizeof(move) * 2);
 //   memset(PREV_PV, 0, MAX_DEPTH * sizeof(move));
 //   for (int i = 0; i < depth; i++) {
@@ -464,7 +474,7 @@
 //       s,
 //       false,
 //       ai_settings);
-// 
+//
 //   move result_move = PV_TABLE[0][0];
 //   board result_board = PV_TABLE_BOARDS[0][0];
 //   team_repetitions new_r = update_team_repetitions(r, result_move, is_black);
@@ -472,13 +482,58 @@
 //   return {result_move, result_board, res, new_r};
 // }
 
-i32 quiesce_white(position_set *positions, score_weights *w, score_state *s, board *b) {
+i32 quiesce_white(position_set *positions, score_weights *w, score_state *s,
+                  board b, u64 position_hash, int ply) {
+
+  // assert we don't exceed a generous ply limit to guard against infinite loops 
+  assert(ply < 20);
+
   // check for repetition
+  int position_index;
+  int collision = insert_position(positions, position_hash, &position_index);
+  if (collision) {
+    // we consider the position a draw, and thus score it 0
+    return 0;
+  }
 
-// white to move, so we score for white
-// This is sort of like a null move score
-  i32 stand_pat = white_score(w, s, b);
+  // white to move, so we score for white
+  // This is sort of like a null move score
+  i32 static_eval = white_score(w, s, &b);
+
+
+  // generate capture moves 
+  layer capture_dests = find_capture_destinations(LAYER_OR(b.white, b.king), b.black, king_board_occ(b));
+  layer capture_dests_r = find_capture_destinations(LAYER_OR(b.white_r, b.king_r), b.black_r, king_board_occ_r(b));
+
+  // shared move memony
+  move ms[335];
+  layer ls[335];
+  layer ls_r[335];
+  int total;
+
+  // generate capture moves for king
+  moves_to_king_impl(
+      capture_dests,
+      capture_dests_r,
+      b.king,
+      b.king_r,
+      king_board_occ(b),
+      king_board_occ_r(b),
+      ms,
+      ls,
+      ls_r,
+      &total);
   
+  // iterate
+
+  // generate capture moves for paws
+
+  // iterate 
+
+  // generate king escapes
+
+  // iterate 
+
+  // remove the position from the set as we exit
+  delete_position(positions, position_index);
 }
-
-
